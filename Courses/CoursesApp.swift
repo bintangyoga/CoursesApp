062492a2198cff1241f24215dbc4ac9ed0ba147d
Bintang Yoga Pamungkas <bintang.pamungkas@stockbit.com>
2022-09-21 15:03:10 +0700

    Initial Commit

---
+++ Courses/CoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesApp.swift
+//  Courses
+//
+//  Created by Bintang Yoga on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
